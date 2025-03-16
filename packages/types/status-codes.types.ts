c13eeb0e16c45d668ecf1a05521ae3d99f8854ad
dhanushd-27 <dhanushsai2005@gmail.com>
2025-03-16 16:07:26 +0530

    Add: Status Code as Types for more readability

--- packages/types/status-codes.types.ts
+++ packages/types/status-codes.types.ts
@@ -1,9 +1,11 @@
-export enum Status {
-  Success = 200,
-  Failed = 400,
-  Unauthorized = 401,
-  ServerFailed = 500,
-  InvalidData = 422,
-  NotFound = 404,
-  Conflict = 409
-}
+export const Status = {
+  Success: 200,
+  Failed: 400,
+  Unauthorized: 401,
+  ServerFailed: 500,
+  InvalidData: 422,
+  NotFound: 404,
+  Conflict: 409
+} as const;
+
+type Status = typeof Status[keyof typeof Status];
