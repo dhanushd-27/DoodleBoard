31e43cd48b38c3e28ed60a3e83df39bab1694f42
dhanushd-27 <dhanushsai2005@gmail.com>
2025-03-24 19:05:24 +0530

    feat: Handle WS Message Event

--- apps/doodbleboard-web/src/utils/socket/onMessage.ts
+++ apps/doodbleboard-web/src/utils/socket/onMessage.ts
@@ -4,13 +4,62 @@ import toast from "react-hot-toast";
 // user joined
 // user left
 
-const handleMessage = () => {
-  
+const handleMessage = ({ message }: { message: string }) => {
+  try {
+    const parsedData = JSON.parse(message);
+    
+    switch(parsedData.event) {
+      case "share": {
+        return 1;
+      }
+
+      case "user_joined": {
+        return 2;
+      }
+
+      case "user_left": {
+        return 3;
+      }
+
+      default: {
+        return 0;
+      }
+    }
+    // return shapeData;
+  } catch (error) {
+    const e = error as ErrorEvent;
+    console.log(e.message)
+    return null;
+  }
 }
 
 export function onMessage(event: MessageEvent<any>, exisitedShapes: string[]) {
   try {
     const parsedData = JSON.parse(event.data);
+
+    const eventType = handleMessage({ message: event.data });
+
+    if(!eventType) {
+      toast.error("Something went wrong");
+      return;
+    }
+
+    switch(eventType) {
+      case 1: {
+        toast.success("Shape data received");
+        break;
+      }
+
+      case 2: {
+        toast.success("User joined");
+        return;
+      }
+
+      case 3: {
+        toast.success("User left");
+        return;
+      }
+    }
     const shapeData = {
       type: parsedData.type,
       payload: JSON.parse(parsedData.payload)
@@ -20,6 +69,5 @@ export function onMessage(event: MessageEvent<any>, exisitedShapes: string[]) {
   } catch (error) {
     const e = error as ErrorEvent;
     console.log(e)
-    toast.error("Invalid data" + event.data);
   }
 }
