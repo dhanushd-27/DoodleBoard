60a0a8f524ee4d759a0a32e85c453b518d196010
dhanushd-27 <dhanushsai2005@gmail.com>
2025-03-28 17:20:49 +0530

    Fix: Sign Up Page using Shadcn UI Components

--- apps/doodbleboard-web/src/app/login/page.tsx
+++ apps/doodbleboard-web/src/app/login/page.tsx
@@ -1,39 +1,8 @@
 import { LoginForm } from '@/components/Login/Login';
 
-
 export default function Login() {
 
   return (
-    // <div className="flex flex-col gap-6">
-    //   <div className="w-full max-w-md p-8 bg-gray-800 shadow-lg rounded-2xl">
-    //     <h2 className="text-white text-2xl font-bold text-center mb-6">Login</h2>
-    //     <form onSubmit={ handleSubmit }>
-    //       <div className="mb-4">
-    //         <label className="block text-gray-400 mb-2">Email</label>
-    //         <input
-    //           type="email"
-    //           className="w-full p-3 bg-gray-700 text-white rounded-lg focus:ring-2 focus:ring-blue-500"
-    //           value={email}
-    //           onChange={(e) => setEmail(e.target.value)}
-    //           required
-    //         />
-    //       </div>
-    //       <div className="mb-4">
-    //         <label className="block text-gray-400 mb-2">Password</label>
-    //         <input
-    //           type="password"
-    //           className="w-full p-3 bg-gray-700 text-white rounded-lg focus:ring-2 focus:ring-blue-500"
-    //           value={password}
-    //           onChange={(e) => setPassword(e.target.value)}
-    //           required
-    //         />
-    //       </div>
-    //       <button type="submit" className="w-full bg-blue-500 hover:bg-blue-600" onClick={ handleSubmit }>
-    //         Login
-    //       </button>
-    //     </form>
-    //   </div>
-    // </div>
     <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-3xl">
         <LoginForm />
---
+++ apps/doodbleboard-web/src/components/Signup/Signup.tsx
@@ -0,0 +1,135 @@
+"use client"
+
+import placeholder from "@/assets/placeholder.jpg"
+import React from 'react'
+import { signUpFunc } from '@/actions/authActions/signup';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import toast from 'react-hot-toast';
+import { Button } from "@/components/ui/button"
+import { Card, CardContent } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import Image from "next/image"
+import Link from 'next/link';
+
+
+export default function SignupForm() {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if(!username || !email || !password) {
+      toast.error("All values are must be provided");
+      return;
+    }
+    
+    const status = await signUpFunc({ username, email, password});
+
+    console.log(status);
+
+    switch (status) {
+      case 201: 
+        toast.success("Sign up successful");
+        router.push('/login');
+        break;
+      
+      case 403:
+        toast.error("User Already Exists");
+        router.push('/');
+        break;
+
+      case 400:
+        toast.error("Bad Request");
+        router.push('/');
+        break;
+
+      case 500:
+        toast.error("Internal Server Error");
+        router.push('/');
+        break;
+
+      default:
+        toast.error("Unexpected error occurred");
+        router.push('/');
+        break;
+    }
+  };
+  
+  return (
+    <div className="flex flex-col gap-6">
+      <Card className="overflow-hidden">
+        <CardContent className="grid p-0 md:grid-cols-2">
+          <form className="p-6 md:p-8" onSubmit={ handleSubmit }>
+            <div className="flex flex-col gap-6">
+              <div className="flex flex-col items-center text-center">
+                <h1 className="text-2xl font-bold">Welcome back</h1>
+                <p className="text-balance text-muted-foreground">
+                  Login to your Acme Inc account
+                </p>
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="username">Username</Label>
+                <Input
+                  id="username"
+                  type="text"
+                  placeholder="Joe Root"
+                  onChange={ (e) => {
+                    setUsername(e.target.value);
+                  }}
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  onChange={ (e) => {
+                    setEmail(e.target.value);
+                  } }
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="password">Password</Label>
+                <Input 
+                  id="password" 
+                  type="password" 
+                  required 
+                  onChange={ (e) => setPassword(e.target.value) } 
+                  placeholder="********"
+                  />
+              </div>
+              <Button type="submit" className="w-full">
+                Login
+              </Button>
+              <div className="text-center text-sm">
+                Don&apos;t have an account?{" "}
+                <Link href={'/login'} className="underline underline-offset-4">
+                  Login
+                </Link>
+              </div>
+            </div>
+          </form>
+          <div className="relative hidden bg-muted md:block mr-8 rounded-2xl">
+            <Image
+              src={ placeholder }
+              alt="Image"
+              className="absolute inset-0 h-full object-cover rounded-2xl"
+            />
+          </div>
+        </CardContent>
+      </Card>
+      <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 hover:[&_a]:text-primary">
+        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
+        and <a href="#">Privacy Policy</a>.
+      </div>
+    </div>
+  )
+}
