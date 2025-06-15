2d74dbe9097f70088de228153e81014e3b203a21
dhanushd-27 <dhanushsai2005@gmail.com>
2025-06-15 22:50:55 +0530

    Update global css

--- apps/doodbleboard-web/src/app/v2/(auth)/layout.tsx
+++ apps/doodbleboard-web/src/app/v2/(auth)/layout.tsx
@@ -5,5 +5,5 @@ export default function AuthLayout({
 }: {
   children: React.ReactNode
 }) {
-  return <section className='bg-background'>{children}</section>
+  return <section className='bg-v2-background'>{children}</section>
 }
