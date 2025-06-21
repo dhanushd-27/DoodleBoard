5108243e4ca4977091e4294573b0a3852fcba6d0
Dhanush <dhanushsai2005@gmail.com>
2025-06-21 21:56:58 +0530

    Add Custom gradient colrs

--- apps/doodbleboard-web/src/app/v2/(auth)/layout.tsx
+++ apps/doodbleboard-web/src/app/v2/(auth)/layout.tsx
@@ -13,7 +13,7 @@ export default function AuthLayout({
       <div className='flex gap-2 items-center justify-start'>
         <Image src={'/logo.png'} alt='app-logo' height={40} width={90}/>
       </div>
-      <div className="flex flex-col gap-4 items-center justify-center flex-1 text-[var(--color-v2-primary)] max-w-full overflow-auto">
+      <div className="flex flex-col gap-4 items-center justify-center flex-1 text-[var(--color-text-base)] max-w-full overflow-auto">
         {children}
       </div>
     </section>
--- apps/doodbleboard-web/src/app/v2/(auth)/login/page.tsx
+++ apps/doodbleboard-web/src/app/v2/(auth)/login/page.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 import { useForm } from 'react-hook-form';
 import { loginSchema, logIn } from '@repo/types/auth';
 import { zodResolver } from '@hookform/resolvers/zod';
+import Image from 'next/image';
 
 export default function Login() {
   const {
@@ -24,21 +25,28 @@ export default function Login() {
   };
 
   return (
-    <div className="w-full max-w-md p-6 bg-[var(--color-v2-primary)] rounded-lg shadow-lg">
+    <div className="w-full max-w-md py-8 px-10 bg-[var(--color-text-base)] rounded-4xl flex flex-col gap-4 items-center justify-center border-2 border-text-light shadow-lg shadow-text-strong/30">
+      <div className='flex items-center justify-center rounded-xl shadow-md p-3 shadow-base/50'>
+        <Image src={'/login.png'} alt='app-logo' height={30} width={30}/>
+      </div>
+
+      <h4 className='text-base'>Log in with Email</h4>
+      <p className='text-base'>Welcome Back!</p>
+
       <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4 w-full">
-        <label htmlFor='email' className='text-sm font-medium text-[var(--color-v2-background)]'>Email</label>
+        <label htmlFor='email' className='text-sm font-medium text-[var(--color-base)]'>Email</label>
         <input 
           type='email'
-          className='w-full p-2 border rounded-md bg-[var(--color-v2-background)] text-[var(--color-v2-primary)] placeholder:text-[var(--color-v2-primary)] border-[var(--color-v2-secondary)] focus:outline-none focus:ring-2 focus:ring-[var(--color-v2-secondary)]'
+          className='w-full p-2 border rounded-md bg-[var(--color-base)] text-[var(--color-text-base)] placeholder:text-[var(--color-text-base)] border-[var(--color-text-strong)] focus:outline-none focus:ring-2 focus:ring-[var(--color-text-strong)]'
           placeholder='Enter your email'
           {...register('email', { required: true })}
         />
         {errors.email && <p className='text-red-500 text-sm'>Email is required</p>}
         
-        <label htmlFor='password' className='text-sm font-medium text-[var(--color-v2-background)]'>Password</label>
+        <label htmlFor='password' className='text-sm font-medium text-[var(--color-base)]'>Password</label>
         <input 
           type='password'
-          className='w-full p-2 border rounded-md bg-[var(--color-v2-background)] text-[var(--color-v2-primary)] placeholder:text-[var(--color-v2-primary)] border-[var(--color-v2-secondary)] focus:outline-none focus:ring-2 focus:ring-[var(--color-v2-secondary)]'
+          className='w-full p-2 border rounded-md bg-[var(--color-base)] text-[var(--color-text-base)] placeholder:text-[var(--color-text-base)] border-[var(--color-text-strong)] focus:outline-none focus:ring-2 focus:ring-[var(--color-text-strong)]'
           placeholder='Enter your password'
           {...register('password', { required: true })} 
         />
@@ -46,7 +54,7 @@ export default function Login() {
         
         <button 
           type='submit'
-          className='w-full p-2 bg-[var(--color-v2-secondary)] text-[var(--color-v2-primary)] rounded-md hover:bg-opacity-90 transition-colors duration-200'
+          className='w-full p-2 bg-[var(--color-text-strong)] text-[var(--color-text-base)] rounded-md hover:bg-opacity-90 transition-colors duration-200'
         >
           Login
         </button>
