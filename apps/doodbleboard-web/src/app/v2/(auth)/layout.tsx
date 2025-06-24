92fcc0f7e1af2fb6f02187b00efa9b8706ca3172
Dhanush <dhanushsai2005@gmail.com>
2025-06-24 13:50:15 +0530

    Add: Noise to login page

--- apps/doodbleboard-web/src/app/v2/(auth)/layout.tsx
+++ apps/doodbleboard-web/src/app/v2/(auth)/layout.tsx
@@ -8,12 +8,12 @@ export default function AuthLayout({
   children: React.ReactNode
 }) {
   return (
-    <section className="h-full w-full flex flex-col flex-1 overflow-hidden">
+    <section className="bg-base h-screen px-17 py-8 relative -z-10 overflow-hidden noise">
       {/* logo div is below*/}
       <div className='flex gap-2 items-center justify-start'>
         <Image src={'/logo.png'} alt='app-logo' height={40} width={90}/>
       </div>
-      <div className="flex flex-col gap-4 items-center justify-center flex-1 text-[var(--color-text-base)] max-w-full overflow-auto">
+      <div className="flex flex-col gap-4 items-center justify-center flex-1 text-[var(--color-text-base)] overflow-auto">
         {children}
       </div>
     </section>
