c4091cc4bd47dfa4ef7fa877f62745e647057b2b
Dhanush <dhanushsai2005@gmail.com>
2025-06-26 16:10:17 +0530

    Update: Login Home page

--- apps/doodbleboard-web/src/app/v2/(auth)/layout.tsx
+++ apps/doodbleboard-web/src/app/v2/(auth)/layout.tsx
@@ -13,10 +13,10 @@ export default function AuthLayout({
       <div className='flex gap-2 items-center justify-start'>
         <Image src={'/logo.png'} alt='app-logo' height={40} width={90}/>
       </div>
-      <div className="flex flex-col gap-4 items-center justify-center flex-1 text-[var(--color-text-base)] overflow-auto">
-        <div className='absolute -bottom-60 -left-60 w-100 h-100 bg-primary rounded-full -z-10 blur-[600px] '></div>
+      <div className="flex flex-col gap-4 items-center justify-center flex-1 text-[var(--color-text-base)]">
+        <div className='absolute -bottom-90 -left-90 w-40 h-200 rotate-45 bg-primary rounded-full -z-10 blur-[200px] '></div>
         {/* <div className='absolute -top-90 right-0 w-100 h-100 bg-subtle rounded-full -z-10 blur-[600px] '></div> */}
-        <div className='absolute top-70 right-50 w-100 h-100 bg-muted rounded-full -z-10 blur-[600px] '></div>
+        <div className='absolute  w-7/12 h-11/12 -bottom-70 -right-70 rounded-full -z-10 bg-linear-to-tl from-primary/40 from-10% via-subtle/40 via-30% to-base to-80% blur-[200px]'></div>
         {children}
       </div>
     </section>
--- apps/doodbleboard-web/src/app/v2/(auth)/login/page.tsx
+++ apps/doodbleboard-web/src/app/v2/(auth)/login/page.tsx
@@ -4,7 +4,6 @@ import React from 'react'
 import { useForm } from 'react-hook-form';
 import { loginSchema, logIn } from '@repo/types/auth';
 import { zodResolver } from '@hookform/resolvers/zod';
-import Image from 'next/image';
 
 export default function Login() {
   const {
@@ -25,29 +24,26 @@ export default function Login() {
   };
 
   return (
-    <div className="w-full max-w-md py-8 px-10 flex flex-col gap-4 items-center justify-center text-primary ">
-      <div className='flex items-center justify-center rounded-xl'>
-        <Image src={'/login.png'} alt='app-logo' height={30} width={30} className="invert"/>
+    <div className="w-full max-w-md py-8 px-10 flex flex-col gap-6 items-center justify-center text-primary rounded-4xl shadow-lg shadow-subtle/50 bg-transparent backdrop-blur-sm backdrop-saturate-100 bg-clip-border mt-32 border border-white/20">
+      <div className='flex flex-col gap-2 items-center justify-center w-full text-center px-2'>
+        <h4 className='font-semibold text-xl'>Sign in with email</h4>
+        <p className='text-primary/70 text-sm'>Online whiteboard helps you to get your idea out there,
+        co-create solutions and show it to others.</p>
       </div>
 
-      <h4 className=''>Log in with Email</h4>
-      <p className=''>Welcome Back!</p>
-
       <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4 w-full">
-        <label htmlFor='email' className='text-sm font-medium'>Email</label>
         <input 
           type='email'
           className='w-full p-2 border rounded-md bg-[var(--color-base)]'
-          placeholder='Enter your email'
+          placeholder='Email'
           {...register('email', { required: true })}
         />
         {errors.email && <p className='text-red-500 text-sm'>Email is required</p>}
         
-        <label htmlFor='password' className='text-sm font-medium'>Password</label>
         <input 
           type='password'
           className='w-full p-2 border rounded-md bg-[var(--color-base)]'
-          placeholder='Enter your password'
+          placeholder='Password'
           {...register('password', { required: true })} 
         />
         {errors.password && <p className='text-red-500 text-sm'>Password is required</p>}
