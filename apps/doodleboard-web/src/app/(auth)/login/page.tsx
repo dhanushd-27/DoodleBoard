14c34d8295d078b40b452a63b66ef0ac86d8ee72
Dhanush <dhanushsai2005@gmail.com>
2025-06-29 12:11:23 +0530

    Update Doodleboard App Frontend

---
+++ apps/doodleboard-web/src/app/(auth)/login/page.tsx
@@ -0,0 +1,93 @@
+'use client'
+
+import React from 'react'
+import { useForm } from 'react-hook-form';
+import { loginSchema, logIn } from '@repo/types/auth';
+import { zodResolver } from '@hookform/resolvers/zod';
+import Image from 'next/image';
+import Link from 'next/link';
+import BackButton from '@/components/auth/BackButton';
+
+export default function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<logIn>({
+    resolver: zodResolver(loginSchema),
+  });
+
+
+  const onSubmit: (values: logIn) => void = (values) => {
+    if (values) {
+      console.log(values);
+    } else {
+      console.log("hello");
+    }
+  };
+
+  return (
+    <div className="mx-auto my-auto w-1/4 flex-1 flex flex-col gap-12 items-center justify-center text-primary">
+      <BackButton/>
+      <div className='flex gap-2 items-center justify-start'>
+        <Image src={'/logo.png'} alt='app-logo' height={40} width={90}/>
+      </div>
+      <div className='flex flex-col gap-2 items-center justify-center w-full text-center px-4'>
+        <h4 className='font-medium text-3xl'>Yooo, welcome back!</h4>
+        <p className='text-primary/70 text-sm tracking-wide'>First time here? <Link href='/signup' className='text-primary hover:underline transition-all duration-300'>Sign up for free</Link></p>
+      </div>
+      
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4 w-full px-4">
+        <div className='flex flex-col gap-2'>
+          <input 
+            type='email'
+            className='w-full px-3 py-2 border rounded-md bg-black/20 border-subtle/50 placeholder:text-sm focus:border-subtle focus:outline-none ease-in-out transition-all duration-300'
+            placeholder='Your email'
+            {...register('email', { required: true })}
+          />
+          {errors.email && <p className='text-subtle text-sm px-1'>Email is required</p>}
+        </div>
+        
+        <div className='flex flex-col gap-2'>
+          <input 
+            type='password'
+            className='w-full px-3 py-2 border rounded-md bg-black/20 border-subtle/50 placeholder:text-sm focus:border-subtle focus:outline-none ease-in-out transition-all duration-300'
+            placeholder='Your password'
+            {...register('password', { required: true })} 
+          />
+          {errors.password && <p className='text-subtle text-sm px-1'>Password is required</p>}
+        </div>
+        
+        <button 
+          type='submit'
+          className='w-full p-2 bg-primary text-base font-medium rounded-lg hover:scale-95 transition-all duration-300'
+        >
+          Sign in
+        </button>
+      </form>
+
+      <div className='flex flex-col items-center justify-center gap-6 w-full'>
+        <div className='flex gap-1.5 w-full items-center justify-center px-12'>
+          <div className='bg-primary/30 py-[0.1px] flex-1'/>
+          <p className='text-xs text-primary/80'>Or sign in with</p>
+          <div className='bg-primary/30 py-[0.1px] flex-1'/>
+        </div>
+        <div className='flex w-full gap-4 justify-center items-center px-12'>
+          <div className='py-2 px-4 bg-white items-center justify-center rounded-md'>
+            <Image className='' src={'/google.png'} height={20} width={20} alt='google icon'></Image>
+          </div>
+          <div className='py-2 px-4 bg-white items-center justify-center rounded-md'>
+            <Image className='' src={'/twitter.png'} height={20} width={20} alt='twitter icon'></Image>
+          </div>
+          <div className='py-2 px-4 bg-white items-center justify-center rounded-md'>
+            <Image className='' src={'/apple.png'} height={20} width={20} alt='apple icon'></Image>
+          </div>
+        </div>
+      </div>
+
+      <footer className='text-xs px-12 text-center'>
+        <p className='text-primary/70 text-sm tracking-wide'>Curious about how this works? ⭐️&nbsp; <Link href='https://github.com/dhanushd-27/DoodleBoard' target='_blank' className='text-primary hover:underline transition-all duration-300 text-xs'>View the code on GitHub</Link></p>
+      </footer>
+    </div>
+  )
+}
